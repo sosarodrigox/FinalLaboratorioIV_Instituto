a433cd545e4575b4b13b1a7b6a6dfd9c654809dd
sosarodrigox <sosarodrigox@gmail.com>
2023-06-30 09:50:45 -0300

    corregido error de CORS

--- frontend/src/componentes/cursos/cursos-lista.jsx
+++ frontend/src/componentes/cursos/cursos-lista.jsx
@@ -1,49 +1,60 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom'
-import {cursos as data} from '../../cursos.js'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+// import { cursos as data } from '../../cursos.js'
 
 
 function CursosLista() {
 
     const [cursos, setCursos] = useState([])
 
-    useEffect(()=>{
-        setCursos(data)
-    },[])
+    // useEffect(() => {
+    //     setCursos(data)
+    // }, [])
 
-  return (
-    <>
-    <div className="Container-fluid">
-        <h1 className="mt-3 tect-center">Cursos</h1>
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Nombre</th>
-                    <th>Cantidad de alumnos</th>
-                    <th>Fecha inicio</th>
-                    <th>Fecha fin</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    //Por cada elemento voy a hacer una fila, cada uno tiene que tener un "key" unico
-                    //àra eso usamos el id del pais que sabemos que es unico
-                    cursos.map((curso, idx) => (
-                        <tr key={curso.id}>
-                            <td><Link to={"" + curso.id}>{curso.id}</Link></td>
-                            <td>{curso.nombre}</td>
-                            <td>{curso.cantAlumnos}</td>
+    useEffect(() => {
+        getDatos()
+    }, [])
+
+    const getDatos = async () => {
+        let resultado = await axios.get("http://localhost:8000/cursos");
+        console.log(resultado)
+        setCursos(resultado.data);
+    }
+
+    return (
+        <>
+            <div className="Container-fluid">
+                <h1 className="mt-3 tect-center">Cursos</h1>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nombre</th>
+                            <th>Cantidad de alumnos</th>
+                            <th>Fecha inicio</th>
+                            <th>Fecha fin</th>
                         </tr>
-                    ))
-                }
-            </tbody>
-        </table>
-        <button className="btn btn-primary">Agregar curso</button>
+                    </thead>
+                    <tbody>
+                        {
+                            //Por cada elemento voy a hacer una fila, cada uno tiene que tener un "key" unico
+                            //àra eso usamos el id del pais que sabemos que es unico
+                            cursos.map((curso, idx) => (
+                                <tr key={curso.id}>
+                                    <td><Link to={"" + curso.id}>{curso.id}</Link></td>
+                                    <td>{curso.nombre}</td>
+                                    <td>{curso.cantAlumnos}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+                <button className="btn btn-primary">Agregar curso</button>
 
-    </div>
-</>
-  )
+            </div>
+        </>
+    )
 }
 
 export default CursosLista
